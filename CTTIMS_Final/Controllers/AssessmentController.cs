c109a0bb976ff876542faaa26c7f9f5fca4ac171
zaheersani <s@ni1981>
2018-01-25 18:20:12 +0500

    Assessment Working

--- CTTIMS_Final/Controllers/AssessmentController.cs
+++ CTTIMS_Final/Controllers/AssessmentController.cs
@@ -39,9 +39,32 @@ namespace CTTIMS_Final.Controllers
         // GET: /Assessment/Create
         public ActionResult Create()
         {
-            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status");
-            ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName");
-            ViewBag.uID = new SelectList(db.Users, "UID", "UserName");
+            // Select a course for adding marks
+            var r = db.InstructorCourses.Where(x => x.InstructorID == 9
+                                       && x.BatchID == 1 && x.SectionID == 1 && x.CourseID == 3);
+
+            InstructorCours instCourse = null;
+            if (r.Count() > 0)
+            {
+                instCourse = r.ToList()[0];
+            }
+            // proceed only if a course is assigned to instructor
+            IEnumerable<Assessment> assessments = null;
+            if (instCourse != null)
+            {
+                // Get enrollments for filtered batch, section and course
+                var enr = db.Enrollments.Where(x => x.InstructorCoursesID == instCourse.ID);
+                
+                if (enr.Count() > 0)
+                {
+                    assessments = db.Assessments.Where(x => enr.Any(eid => eid.ID == x.EnrollmentID));
+                    return View(assessments.ToList());
+                }
+            }
+
+            //ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status");
+            //ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName");
+            //ViewBag.uID = new SelectList(db.Users, "UID", "UserName");
             return View();
         }
 
@@ -50,19 +73,24 @@ namespace CTTIMS_Final.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="ID,uID,EnrollmentID,A1_Max,A1_Obt,A2_Max,A2_Obt,A3_Max,A3_Obt,A4_Max,A4_Obt,A5_Max,A5_Obt,Q1_Max,Q1_Obt,Q2_Max,Q2_Obt,Q3_Max,Q3_Obt,Mid_Max,Mid_Obt,SendUp_Max,SendUp_Obt,Final_Max,Final_Obt,CreatedOn,ModifiedOn")] Assessment assessment)
+        public ActionResult Create(IEnumerable<Assessment> assessments)
         {
             if (ModelState.IsValid)
             {
-                db.Assessments.Add(assessment);
+                // Update record of each student in assessment table
+                foreach (var asmnt in assessments)
+                {
+                    db.Entry(asmnt).State = EntityState.Modified;
+                }
+                // Finally update database
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status", assessment.EnrollmentID);
-            ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName", assessment.uID);
-            ViewBag.uID = new SelectList(db.Users, "UID", "UserName", assessment.uID);
-            return View(assessment);
+            //ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status", assessment.EnrollmentID);
+            //ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName", assessment.uID);
+            //ViewBag.uID = new SelectList(db.Users, "UID", "UserName", assessment.uID);
+            return View();
         }
 
         // GET: /Assessment/Edit/5
