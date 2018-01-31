a729038a77ba59fbb7f586817747fd7e5c9a215c
zaheersani <s@ni1981>
2018-01-31 20:21:26 +0500

    Attendance Insertion is working

--- CTTIMS_Final/Controllers/AttendanceController.cs
+++ CTTIMS_Final/Controllers/AttendanceController.cs
@@ -10,20 +10,11 @@ using CTTIMS_Final.Models;
 
 namespace CTTIMS_Final.Controllers
 {
-    public class AssessmentController : Controller
+    public class AttendanceController : Controller
     {
         private CTTIDBEntities db = new CTTIDBEntities();
 
-        // GET: /Assessment/
-        public ActionResult Index()
-        {
-            IEnumerable<Assessment> assessments = this.GetAssessments();
-            if(assessments != null)
-                return View(assessments.ToList());
-            return View();
-        }
-
-        private IEnumerable<Assessment> GetAssessments()
+        private IEnumerable<Enrollment> GetEnrollments()
         {
             // Select a course for adding marks
             var r = db.InstructorCourses.Where(x => x.InstructorID == 9
@@ -36,129 +27,140 @@ namespace CTTIMS_Final.Controllers
                 //instCourse.ClassRoom
             }
             // proceed only if a course is assigned to instructor
-            IEnumerable<Assessment> assessments = null;
             if (instCourse != null)
             {
                 // Get enrollments for filtered batch, section and course
                 var enr = db.Enrollments.Where(x => x.InstructorCoursesID == instCourse.ID);
 
-                if (enr.Count() > 0)
-                {
-                    assessments = db.Assessments.Where(x => enr.Any(eid => eid.ID == x.EnrollmentID));
-                    return assessments.ToList();
-                }
+                //if (enr.Count() > 0)
+                //{
+                //    assessments = db.Assessments.Where(x => enr.Any(eid => eid.ID == x.EnrollmentID));
+                //    return assessments.ToList();
+                //}
+                return enr.ToList();
             }
             return null;
         }
 
-        // GET: /Assessment/Details/5
+        // GET: /Attendance/
+        public ActionResult Index()
+        {
+            var attendences = db.Attendences.Include(a => a.Enrollment).Include(a => a.Instructor).Include(a => a.User);
+            return View(attendences.ToList());
+        }
+
+        // GET: /Attendance/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Assessment assessment = db.Assessments.Find(id);
-            if (assessment == null)
+            Attendence attendence = db.Attendences.Find(id);
+            if (attendence == null)
             {
                 return HttpNotFound();
             }
-            return View(assessment);
+            return View(attendence);
         }
 
-        // GET: /Assessment/Create
+        // GET: /Attendance/Create
         public ActionResult Create()
         {
-            IEnumerable<Assessment> assessments = this.GetAssessments();
-            if (assessments != null)
-                return View(assessments.ToList());
+            IEnumerable<Enrollment> enrollments = this.GetEnrollments();
+            if (enrollments != null)
+            {
+                ViewBag.Enrollments = enrollments.ToList();
+                //return View(enrollments.ToList());
+                List<Attendence> aList = new List<Attendence>();
+                foreach (var item in enrollments)
+                {
+                    aList.Add(new Attendence()
+                    {
+                        EnrollmentID = item.ID
+                    });
+                }
+                return View(aList);
+
+            }
             return View();
         }
 
-        // POST: /Assessment/Create
+        // POST: /Attendance/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IEnumerable<Assessment> assessments, string bid)
+        public ActionResult Create(IEnumerable<Attendence> attendenceList)
         {
             if (ModelState.IsValid)
             {
-                // Update record of each student in assessment table
-                foreach (var asmnt in assessments)
-                {
-                    db.Entry(asmnt).State = EntityState.Modified;
-                }
-                // Finally update database
+                db.Attendences.AddRange(attendenceList);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            //ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status", assessment.EnrollmentID);
-            //ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName", assessment.uID);
-            //ViewBag.uID = new SelectList(db.Users, "UID", "UserName", assessment.uID);
             return View();
         }
 
-        // GET: /Assessment/Edit/5
+        // GET: /Attendance/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Assessment assessment = db.Assessments.Find(id);
-            if (assessment == null)
+            Attendence attendence = db.Attendences.Find(id);
+            if (attendence == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status", assessment.EnrollmentID);
-            ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName", assessment.uID);
-            ViewBag.uID = new SelectList(db.Users, "UID", "UserName", assessment.uID);
-            return View(assessment);
+            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status", attendence.EnrollmentID);
+            ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName", attendence.uID);
+            ViewBag.uID = new SelectList(db.Users, "UID", "UserName", attendence.uID);
+            return View(attendence);
         }
 
-        // POST: /Assessment/Edit/5
+        // POST: /Attendance/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,uID,EnrollmentID,A1_Max,A1_Obt,A2_Max,A2_Obt,A3_Max,A3_Obt,A4_Max,A4_Obt,A5_Max,A5_Obt,Q1_Max,Q1_Obt,Q2_Max,Q2_Obt,Q3_Max,Q3_Obt,Mid_Max,Mid_Obt,SendUp_Max,SendUp_Obt,Final_Max,Final_Obt,CreatedOn,ModifiedOn")] Assessment assessment)
+        public ActionResult Edit([Bind(Include="ID,EnrollmentID,uID,ClassRoom,Status,EntryDate,FromTime,ToTime,ModifiedOn,CreatedOn,TopicsCovered")] Attendence attendence)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(assessment).State = EntityState.Modified;
+                db.Entry(attendence).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status", assessment.EnrollmentID);
-            ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName", assessment.uID);
-            ViewBag.uID = new SelectList(db.Users, "UID", "UserName", assessment.uID);
-            return View(assessment);
+            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "ID", "Status", attendence.EnrollmentID);
+            ViewBag.uID = new SelectList(db.Instructors, "ID", "FirstName", attendence.uID);
+            ViewBag.uID = new SelectList(db.Users, "UID", "UserName", attendence.uID);
+            return View(attendence);
         }
 
-        // GET: /Assessment/Delete/5
+        // GET: /Attendance/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Assessment assessment = db.Assessments.Find(id);
-            if (assessment == null)
+            Attendence attendence = db.Attendences.Find(id);
+            if (attendence == null)
             {
                 return HttpNotFound();
             }
-            return View(assessment);
+            return View(attendence);
         }
 
-        // POST: /Assessment/Delete/5
+        // POST: /Attendance/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Assessment assessment = db.Assessments.Find(id);
-            db.Assessments.Remove(assessment);
+            Attendence attendence = db.Attendences.Find(id);
+            db.Attendences.Remove(attendence);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
